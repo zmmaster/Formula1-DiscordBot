9c59e1baca5115834fa1c56ed40665a73d5fc7ba
Zachary Master <zmmaster@buffalo.edu>
2022-03-16 22:14:51 -0400

    Unit tests to test function of season_checker() and rnd_checker() helper functions. Currently all tests pass. The test case test_non_integer_season is commented out because I realized this SyntaxError can't be caught through season_checker(). It is  better to be handled by the bot side as it will be the portion of code that will first intercept the input and be able to handle the exception.

--- tests/unit/test_urlbuilder.py
+++ tests/unit/test_urlbuilder.py
@@ -1,6 +1,7 @@
 import unittest
 
-from ergstapiclient import url_builder
+from datetime import date
+from ergstapiclient import url_builder, earliest_year, season_checker, rnd_checker
 
 
 class TestUrlBuilder(unittest.TestCase):
@@ -11,91 +12,101 @@ class TestUrlBuilder(unittest.TestCase):
 
 
 class TestSeasonFormat(unittest.TestCase):
+    def test_correct_earliest_year(self):
+        year = 1950
+        self.assertEqual(earliest_year, year)
+    
     def test_correct_season_length(self):
-        season = "2021"
-        self.assertTrue()
-
+        test_year = date(2021, 1, 1)
+        season = 2021 
+        self.assertTrue(season_checker(season,False))
+    
     def test_wrong_season_lengthshort(self):
-        season = "2"
-        self.assertFalse()
+        test_year = date(2, 1, 1)
+        season = test_year.year
+        self.assertFalse(season_checker(season,False))
 
     def test_wrong_season_lengthlong(self):
-        season = "20214"
-        self.assertFalse()
-
+       	season = 20222 
+        self.assertFalse(season_checker(season,False))
+    """
+    Handle this exception when input transfered from discord to python, not here 
     def test_non_integer_season(self):
-        season = "2a021"
-        self.assertFalse()
-
-    def test_integer_season(self):
-        season = "2021"
-        self.assertTrue()
-
+        season = 202a
+        self.assertFalse(season_checker(202a,False))
+    """
     def test_season_outofrange_low(self):
-        season = "1930"
-        self.assertFalse()
+        test_year = date(1930, 1, 1)
+        season = test_year.year
+        self.assertFalse(season_checker(season, False))
 
     def test_season_outofrange_high(self):
-        season = "3330"
-        self.assertFalse()
+        test_year = date(3330, 1, 1)
+        season = test_year.year
+        self.assertFalse(season_checker(season, False))
 
     def test_season_inrange(self):
-        season = "1980"
-        self.assertTrue()
+        test_year = date(1980, 1, 1)
+        season = test_year.year
+        self.assertTrue(season_checker(season, False))
 
     def test_season_correctkeyword(self):
         season = "current"
-        self.assertTrue()
+        self.assertTrue(season_checker(season))
 
     def test_season_wrongkeyword(self):
         season = "cuurent"
-        self.assertFalse()
+        self.assertFalse(season_checker(season))
+
+    def test_season_emptystr(self):
+        season = ""
+        self.assertFalse(season_checker(season))
 
 
 class TestRndFormat(unittest.TestCase):
     def test_correct_rnd_length_twodigits(self):
-        rnd = "12"
-        self.assertTrue()
+        rnd = 12
+        self.assertTrue(rnd_checker(rnd, False))
 
     def test_correct_rnd_length_onedigits(self):
-        rnd = "2"
-        self.assertTrue()
+        rnd = 2
+        self.assertTrue(rnd_checker(rnd, False))
 
     def test_wrong_rnd_lengthshort(self):
         rnd = ""
-        self.assertFalse()
+        self.assertFalse(rnd_checker(rnd))
 
     def test_wrong_rnd_lengthlong(self):
-        rnd = "20214"
-        self.assertFalse()
+        rnd = 20214
+        self.assertFalse(rnd_checker(rnd, False))
 
     def test_non_integer_rnd(self):
         rnd = "a"
-        self.assertFalse()
+        self.assertFalse(rnd_checker(rnd))
 
     def test_integer_rnd(self):
-        rnd = "21"
-        self.assertTrue()
+        rnd = 21
+        self.assertTrue(rnd_checker(rnd, False))
 
     def test_rnd_outofrange_low(self):
-        rnd = "0"
-        self.assertFalse()
+        rnd = 0
+        self.assertFalse(rnd_checker(rnd, False))
 
     def test_rnd_outofrange_high(self):
-        rnd = "25"
-        self.assertFalse()
+        rnd = 25
+        self.assertFalse(rnd_checker(rnd, False))
 
     def test_rnd_inrange(self):
-        rnd = "6"
-        self.assertTrue()
+        rnd = 6
+        self.assertTrue(rnd_checker(rnd, False))
 
     def test_rnd_correctkeyword(self):
         rnd = "last"
-        self.assertTrue()
+        self.assertTrue(rnd_checker(rnd))
 
     def test_rnd_wrongkeyword(self):
         rnd = "netx"
-        self.assertTrue()
+        self.assertFalse(rnd_checker(rnd))
 
 
 class TestFormatHelper(unittest.TestCase):
